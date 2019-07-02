aea78b3391c0739ce50a9e235507c15c78f924bd
diegoveloper <diego.velasquez.lopez@gmail.com>
2019-07-02 00:52:42 -0500

    space for HOURLY chart was increased

--- lib/src/bezier_chart_widget.dart
+++ lib/src/bezier_chart_widget.dart
@@ -337,7 +337,7 @@ class BezierChartState extends State<BezierChart>
           constraints.maxWidth - 2 * horizontalPadding;
     } else {
       if (scale == BezierChartScale.HOURLY) {
-        horizontalSpacing = constraints.maxWidth / 10;
+        horizontalSpacing = constraints.maxWidth / 7;
         return _xAxisDataPoints.length * (horizontalSpacing * _currentScale) -
             horizontalPadding / 2;
       } else if (scale == BezierChartScale.WEEKLY) {
